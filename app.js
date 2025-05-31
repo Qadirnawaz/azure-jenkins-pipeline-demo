92c3565da05f940c97b1a1dbed68ae78f17be12e
Qadir Nawaz Khan <musafark463@gmail.com>
2025-06-01 00:47:52 +0500

    Initial commit: Simple Node.js app

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000; // Or any port you prefer
+
+app.get('/', (req, res) => {
+  res.send('Hello from Jenkins CI/CD on Azure! V1'); // We'll change V1 later to test CI/CD
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
